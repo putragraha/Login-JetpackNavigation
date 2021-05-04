11872df1e5142939f149c0c42e47e2fd8e43fce5
accenture.putra <putra.nugraha@accenture.com>
2021-05-04 10:12:18 +0700

    create deeplink with args

--- app/src/main/java/nsystem/loginjetpacknavigation/LoginFragment.kt
+++ app/src/main/java/nsystem/loginjetpacknavigation/LoginFragment.kt
@@ -7,11 +7,14 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavDirections
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import nsystem.loginjetpacknavigation.databinding.FragmentLoginBinding
 
 class LoginFragment: Fragment() {
 
     private lateinit var binding: FragmentLoginBinding
+
+    private val args: LoginFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,7 +28,7 @@ class LoginFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        binding.etUsername.setText(args.username)
         binding.btnLogin.setOnClickListener {
             val username = binding.etUsername.text.toString()
             val action: NavDirections = LoginFragmentDirections.actionLoginFragmentToHomeFragment(
